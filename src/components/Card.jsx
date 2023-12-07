6bd72839ac1de797b187ffaeaeb498fe34aa58b1
Rishabh_Pal_Singh <rishabhpalsingh7@gmail.com>
2023-12-07 18:45:58 +0530

    implemented drag feature for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { PiDownloadSimpleBold } from "react-icons/pi";
+import { CgCloseO } from "react-icons/cg";
+import { motion } from "framer-motion"
 
-const Card = () => {
+function Card({data }){
   return (
-    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 overflow-hidden text-white px-7 py-10'>
+    <motion.div drag whileDrag={{scale: 1.2}} className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 overflow-hidden text-white px-7 py-10'>
     <FaRegFileAlt />
     <p className='text-sm mt-5 leading-tight font-semibold' 
-    >Free from the veil: Your Life is like a game and you are in a concious dream.</p>
+    >{data.desc}</p>
     <div className='footer absolute bottom-0 w-full   left-0'>
         <div className='flex items-center mb-5 py-3 px-8 justify-between '>
-            <h5>.5mb</h5>
+            <h5>{data.filesize}</h5>
             <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center'>
-            <PiDownloadSimpleBold size=".9em" />
+            {data.close ? <CgCloseO /> : <PiDownloadSimpleBold size=".9em" />}
+            
             </span>
         </div>
-        <div className='flex items-center justify-center tag w-full py-4 bg-green-600 '>
-            <h3 className='text-sm font-semibold'>Download Now</h3>
-        </div>
+         {
+          data.tag.isOpen && (
+            <div className={`flex items-center ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} justify-center tag w-full py-4 `}>
+               <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+            </div>
+          ) 
+         }
+
+        
     </div>
-    </div>
+    </motion.div>
 
   )
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from './Card'
-const Foreground = () => {
+
+function Foreground () {
+
+ 
+
+  const data = [
+    {
+    desc:"Break the veil: Your Life is like a game which is a concious dream.",
+    filesize: "1.2MB",
+    close: true ,
+    tag: {isOpen: true, tagTitle: "Download Now", tagColor: "Black"},
+  },
+  {
+    desc:"The world is a simulation. A simulation of your thoughts.",
+    filesize: ".3MB",
+    close: true ,
+    tag: {isOpen: true, tagTitle: "TAG 2", tagColor: "blue"},
+  },
+  {
+    desc:"All these memories are in a single membrane as if in a sack that you carry which in turn encumbres you.",
+    filesize: ".2MB",
+    close: true ,
+    tag: {isOpen: true, tagTitle: "Tag 3", tagColor: "Black"},
+  },
+ ];
   return (
-    <div className= 'fixed top-0 left-0 z-[3] w-full h-full'>
-    <Card />
-    <Card />
+    <div  className= 'fixed top-0 left-0 z-[3] w-full h-full flex gap-7 p-5 flex-wrap'>
+     {data.map((item, index)=>(
+      <Card data={item} />
+     ))}
     </div>
   )
 }
